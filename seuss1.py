5975212c5681560eef797f6ece7c2534df4de33b
Mike Smith <mike_smith@harvard.edu>
2024-06-23 14:17:10 -0400

    Remove old solution to this chapter's problem

--- seuss1.py
+++
@@ -1,7 +0,0 @@
-my_open_book = open('CatInTheHat.txt')
-while True:
-    the_line = my_open_book.readline()
-    print(the_line, end='')
-    if the_line == '':
-        print("The End.")
-        break
